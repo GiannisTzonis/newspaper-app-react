fba8b8f7858688254c4e4619662e3c6167966e14
Giannis Tzonis <tzonis.john@hotmail.com>
2022-09-06 04:35:25 +0300

    added card.js

---
+++ src/components/Card.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+export default function Card(props) {
+    // let badgeText
+    // if (props.item.openSpots === 0) {
+    //     badgeText = "SOLD OUT"
+    // } else if (props.item.location === "Online") {
+    //     badgeText = "ONLINE"
+    // }
+    
+    
+    return (
+        <div className="card">
+            {/* {badgeText && <div className="card--badge">{badgeText}</div>} */}
+            <img src={props.item.imageUrl} className="card--image" />
+            <div className="card--stats">
+                <h2>{props.item.title}</h2>
+                <span>{props.item.location}</span>
+                <span>({props.item.year}) • </span>
+                {/* <span>{props.item.description}</span> */}
+            </div>
+            <p className="card--descipription">{props.item.description}</p>
+            {/* <p className="card--price"><span className="bold">From ${props.item.price}</span> / person</p> */}
+        </div>
+    )
+}
