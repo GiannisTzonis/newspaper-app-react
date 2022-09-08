cc93e732218312b42bdd9e7c1d00ebe8b5a219ca
Giannis Tzonis <tzonis.john@hotmail.com>
2022-09-08 04:09:31 +0300

    data export rdy

--- src/data.js
+++ src/data.js
@@ -1,11 +1,29 @@
 export default [
     {
-        title: "Mount Fuji",
-        location: "Japan",
+        title: "Amsterdam",
+        location: "Netherlands",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        year: "2019",
+        endDate: "24 Jan, 2021",
+        description: "It is the capital and the principal commercial and financial centre of the Netherlands. To the scores of tourists who visit each year, Amsterdam is known for its historical attractions, for its collections of great art, and for the distinctive colour and flavour of its old sections, which have been so well preserved.",
+        imageUrl: "https://unsplash.com/photos/3vQXi_i0P4w"
+    },
+    {
+        title: "Dubai",
+        location: "United Arab Emirates",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
         startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        year: "2004",
+        description: "Dubai is a city and emirate in the United Arab Emirates known for luxury shopping, ultramodern architecture and a lively nightlife scene. Burj Khalifa, an 830m-tall tower, dominates the skyscraper-filled skyline. At its foot lies Dubai Fountain, with jets and lights choreographed to music.",
+        imageUrl: "https://unsplash.com/photos/Fr6zexbmjmc"
+    },
+    {
+        title: "Seville",
+        location: "Spain",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        year: "2016",
+        description: "This beautiful city is located on the Guadalquivir river. Thanks to the many historical highlights, Seville is seen by many people as the most beautiful city of Andalusia and Spain. Conclusion, considering all the Arabic influences, the narrow winding streets, parks and the large historic centre (Barrio de Santa Cruz). Barrio de Triana, a district of Seville is seen as the birthplace of flamenco, the typical Spanish dance and music. Seville is famous for its Mudéjar architecture, which is a mix of Islamic and Catholic architectural styles. After the Catholics defeated the Moors, who had ruled over Seville for more than 500 years, they kept many beautiful buildings intact like the Alcazar Palace. Sometimes they gave it their own Catholic twist, like the cathedral with the Giralda.",
+        imageUrl: "https://unsplash.com/photos/62QRdDoe44M"
     }
 ]
